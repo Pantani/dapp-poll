b7a79e1b8210e77a019f54d7932ec56412a9b93d
Danilo Pantani <danpantani@gmail.com>
2022-04-28 22:38:35 -0300

    pool list component logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  showForm = false
+  showForm = false;
+  polls = [
+    {
+      question: 'Do you like cats, dogs or birds',
+      votes: [0, 5, 6],
+      voted: true,
+      image:
+        'https://uploads.mwp.mprod.getusinfo.com/uploads/sites/29/2021/08/Screen-Shot-2021-08-16-at-5.37.50-AM.png',
+    },
+    {
+      question: 'Do you like human, aliens or animals?',
+      votes: [0, 2, 3],
+      voted: false,
+      image:
+        'https://d2v9ipibika81v.cloudfront.net/uploads/sites/72/431539-PE9O1K-661-1140x684.jpg',
+    },
+  ];
 }
--- src/app/poll-list/poll-list.component.ts
+++ src/app/poll-list/poll-list.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-poll-list',
   templateUrl: './poll-list.component.html',
-  styleUrls: ['./poll-list.component.sass']
+  styleUrls: ['./poll-list.component.sass'],
 })
 export class PollListComponent implements OnInit {
+  @Input() question: string;
+  @Input() votes: number[];
+  @Input() voted: boolean;
+  @Input() pollImage: string;
 
-  constructor() { }
+  numberOfVotes: number;
 
-  ngOnInit(): void {
+  constructor() {
+    this.question = '';
+    this.votes = [];
+    this.voted = false;
+    this.numberOfVotes = 0;
+    this.pollImage = '';
   }
 
+  ngOnInit(): void {
+    if (this.votes.length) {
+      this.numberOfVotes = this.votes.reduce((acc, curr) => {
+        return (acc += curr);
+      });
+    }
+  }
 }
