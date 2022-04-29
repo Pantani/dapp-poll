b99fb9bfe55b11b816ade2d5a9fe4c6f86ca22ca
Danilo Pantani <danpantani@gmail.com>
2022-04-28 22:50:25 -0300

    pool-vote basic template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PollCreateComponent } from './poll-create/poll-create.component';
 import { PollListComponent } from './poll-list/poll-list.component';
+import { PollVoteComponent } from './poll-vote/poll-vote.component';
 
 @NgModule({
-  declarations: [AppComponent, PollCreateComponent, PollListComponent],
+  declarations: [AppComponent, PollCreateComponent, PollListComponent, PollVoteComponent],
   imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/poll-vote/poll-vote.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-poll-vote',
+  templateUrl: './poll-vote.component.html',
+  styleUrls: ['./poll-vote.component.sass'],
+})
+export class PollVoteComponent implements OnInit {
+  options = ['Monday', 'Tuesday', 'Wednesday']
+  voteForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.voteForm = this.fb.group({
+      selected: this.fb.control('', [Validators.required]),
+    });
+  }
+
+  ngOnInit(): void {}
+
+  submitForm() {
+    console.log(this.voteForm.value);
+  }
+}
