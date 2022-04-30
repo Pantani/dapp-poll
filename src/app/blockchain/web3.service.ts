c34351b8fab12ec6b3d2a7342f5d0d924ba9651d
Danilo Pantani <danpantani@gmail.com>
2022-04-30 17:28:22 -0300

    web3 service

---
+++ src/app/blockchain/web3.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import Web3 from 'web3';
+import { Contract } from 'web3-eth-contract';
+
+const contractAbi = require('./contractABI.json');
+declare var window: any;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Web3Service {
+  private web3: Web3;
+  private contract: Contract;
+  private contractAddress = '0x55F4998f1f5F4c733BEbF42BEDA2a540Dd5B2041';
+
+  constructor() {
+    if (window.web3) {
+      this.web3 = new Web3(window.ethereum);
+      this.contract = new this.web3.eth.Contract(
+        contractAbi,
+        this.contractAddress
+      );
+
+      window.ethereum.enable().catch((err: Error) => {
+        console.log(err);
+      });
+    } else {
+      console.warn('Metamask not found. Install or enable metamask');
+    }
+  }
+
+  getAccount(): Promise<string> {
+    return this.web3.eth.getAccounts().then((accounts) => accounts[0] || '');
+  }
+
+  async executeTransaction(fnName: string, ...args: any): Promise<void> {
+    const acc = await this.getAccount();
+    this.contract.methods[fnName](...args).send({ from: acc });
+  }
+}
